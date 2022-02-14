2598883a5b72f2cfba0b566e7f351b4790addc07
Mir Md Asif Hossain <asif.hossain@personio.de>
2022-02-14 17:05:04 +0100

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>Hi there! I am a software engineer and I built this site using gatsby.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
